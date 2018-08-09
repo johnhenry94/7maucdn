256f97621d8c4c953302b9aa1f037cad9c587e9b
johnhenry94 <fke5s4jrza@gmail.com>
2018-08-10 00:26:59 +0700

    Add files via upload

---
+++ desktop.js
@@ -0,0 +1,374 @@
+﻿olderLastPublishedTime = null;
+
+function soundToggle(id) {
+    var video = $("#id-" + id + " video")[0];
+    $(video).attr('id', id);
+
+
+    if (video.muted) {
+        video.muted = false;
+        $("#sound-" + id).addClass('on');
+        $("#sound-" + id).removeClass('off');
+
+        $("video[id!='" + id + "']").each(function () {
+            this.muted = true;
+
+            $("#sound-" + this.getAttribute('id')).addClass('off');
+            $("#sound-" + this.getAttribute('id')).removeClass('on');
+        });
+    }
+    else {
+        video.muted = true;
+        $("#sound-" + id).addClass('off');
+        $("#sound-" + id).removeClass('on');
+    }
+
+}
+
+function assignVideoHandler(element) {
+
+    $(element).attr('parsed', true);
+    $(element).attr('loop', true);
+
+    $(element).on('inview', function (event, isInView) {
+
+        var postId = $(this).attr('id');
+
+        if (isInView) {
+            // element is now visible in the viewport
+            if (this.paused == true && this.muted == false) {
+                this.muted = true;
+
+                // Chuyển nút điều khiển âm lượng về chế độ tắt
+                if (postId != "") {
+                    $("#sound-" + postId).addClass('off');
+                    $("#sound-" + postId).removeClass('on');
+                }
+            }
+
+            if ($(this).attr('data-autoplay') == "true") {
+                this.muted = false;
+
+                // Chuyển nút điều khiển âm lượng về chế độ mở
+                if (postId != "") {
+                    $("#sound-" + id).addClass('on');
+                    $("#sound-" + id).removeClass('off');
+                }
+
+            }
+
+            this.play();
+
+        } else {
+            this.pause()
+        }
+    });
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+// Hiển thị bài viết chi tiết Ajax
+function displayAjaxDetail(json) {
+    $("#ajax-detail-container").show();
+    $("#ajax-loading-container").hide();
+
+    var entry = json.entry;
+    var id = entry.id.$t;
+    var url = entry.link[2].href;
+    var category = entry.category[0].term;
+
+    var date = new Date(entry.published.$t);
+
+    var afterVideoAds = "";
+    var bottomAds = "";
+
+    if (category != "video") {
+        afterVideoAds = "";
+
+        $("#mobile-ajax-detail").html("<h1 class='title' id='ajax-title'>" + entry.title.$t + " </h1> <div class='publish-date'> <div class='post-header'> <div class='published'>" + date.toLocaleString() + "</div> </div> </div> <div class='tool top'> <a class='zalo-share zalo-share-button' data-customize='true' data-href='" + url + "' data-layout='icon' data-oaid='579745863508352884' data-type='zalo' href='javascript:;' rel='nofollow'> <i class='spr spr-social-zalo'></i></a> <a class='fb-share' data-href='" + url + "' data-type='facebook' href='javascript:;'> <i class='spr spr-social-fb'></i> </a> </div> <div class='content' id='ajax-content'>" + entry.content.$t + "</div> <div class='tool bottom'> <a class='zalo-share zalo-share-button' data-customize='true' data-href='" + url + "' data-layout='icon' data-oaid='579745863508352884' data-type='zalo' href='javascript:;' rel='nofollow'> <i class='spr spr-social-zalo'></i> <span>zalo</span> </a> <a class='fb-share' data-href='" + url + "' data-type='facebook' href='javascript:;'> <i class='spr spr-social-fb'></i> <span>facebook</span> </a> </div><div id='ajax-fb-comment' ><div class='fb-comments'  data-href='" + url + "' data-numposts='2'></div></div>");
+    }
+    else {
+        $("#mobile-ajax-detail").html("<div class='content' id='ajax-content'>" + entry.content.$t + "</div> <h1 class='video-title' id='ajax-title'>" + entry.title.$t + " </h1> <div class='publish-date'> <div class='post-header'> <div class='published'>" + date.toLocaleString() + "</div> </div> </div><div class='tool bottom'> <a class='zalo-share zalo-share-button' data-customize='true' data-href='" + url + "' data-layout='icon' data-oaid='579745863508352884' data-type='zalo' href='javascript:;' rel='nofollow'> <i class='spr spr-social-zalo'></i> <span>zalo</span> </a> <a class='fb-share' data-href='" + url + "' data-type='facebook' href='javascript:;'> <i class='spr spr-social-fb'></i> <span>facebook</span> </a> </div><div id='ajax-fb-comment' ><div class='fb-comments'  data-href='" + url + "' data-numposts='2'></div></div>");
+    }
+
+
+
+    // Quảngg cáo chèn giữa bài viết
+    $("<center class='mobile-footer-ads'><ins class='adsbygoogle' data-ad-client='ca-pub-8618945885313646' data-ad-slot='9417749014' style='display:inline-block;width:300px;height:250px'></ins></center><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>").insertAfter($($("#mobile-ajax-detail p")[Math.ceil($("#mobile-ajax-detail p").length / 2)]));
+
+
+    if (typeof ZaloSocialSDK != 'undefined') {
+        ZaloSocialSDK.reload();
+    }
+
+    FB.XFBML.parse(document.getElementById("ajax-fb-comment"));
+}
+
+isDetailAjaxPage = false; // Xác định đang mở xem chi tiết = ajax
+$(document).ready(function () {
+    // construct an instance of Headroom, passing the element
+    var headroom = new Headroom(document.getElementById("sticker"), {
+        "offset": 45,
+        "tolerance": 5,
+        "classes": {
+            "initial": "animated",
+            "pinned": "slideDown",
+            "unpinned": "slideUp"
+        }
+    });
+    // initialise
+    headroom.init();
+
+    $("video:not([parsed='true'])").each(function () {
+        assignVideoHandler(this);
+    });
+
+    getArticleStatistics();
+    listLoadMore();
+});
+
+// Chia sẽ bài viết qua facebook
+$(document).on('click', 'a[class*="share-facebook"]', function (e) {
+    e.preventDefault();
+    var button = this;
+    window.FB.ui({
+        method: "share",
+        href: $(this).attr('href')
+    }, function (e) {
+        alert("Cảm ơn bạn đã chia sẽ <3");
+        $(button).addClass('has-share')
+    });
+
+});
+
+
+$(document).on('click', '#post-list a', function (e) {
+    if (getParameterByName("m") == "1") {
+        e.preventDefault();
+
+        isDetailAjaxPage = true;
+
+        $("#ajax-loading-container").show();
+        $('#detail-modal').show();
+        $.get('/feeds/posts/default/' + $(this).attr('data-id') + '?alt=json-in-script&callback=displayAjaxDetail');
+
+        $("#pure-body").hide();
+
+        history.pushState(null, null, $(this).attr('href') + "?m=1");
+    }
+});
+
+window.onpopstate = function (event) {
+    video[0].pause();
+
+    $("#pure-body").show();
+    $('#detail-modal').hide();
+
+    $("#ajax-detail-container").hide();
+    $("#ajax-loading-container").hide();
+
+    isDetailAjaxPage = false;
+
+    $("#mobile-ajax-detail").html("");
+};
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function extractThumbnail(content) {
+    var thumbnail = "";
+
+    var findUrl = false;
+    for (var i = 0; i < 500; i++) {
+        if (!findUrl) {
+            if (content[i] == "'") {
+                findUrl = true;
+            }
+
+        } else {
+
+            if (content[i] == "'" && findUrl) {
+                break;
+            }
+
+            thumbnail += content[i];
+        }
+    }
+
+    return thumbnail;
+}
+
+
+
+loadTime = 0;
+function displayDesktopSuggest(json) {
+    var html = "";
+    for (var i = 0; i < json.feed.entry.length; i++) {
+        var entry = json.feed.entry[i];
+        var title = entry.title.$t;
+        var url = entry.link[2].href;
+        var thumbnail = extractThumbnail(entry.content.$t);
+
+        html += "<article> <a href='" + url + "'> <img src='" + thumbnail + "' alt='" + title + "' /></a> <div> <a href='" + url + "' class='title'>" + title + "</a> </div> </article>";
+    }
+
+    document.getElementById('suggest-list').innerHTML = html;
+
+    $("video:not([parsed='true'])").each(function () {
+        assignVideoHandler(this);
+    });
+
+}
+
+
+loadTime = 0;
+function requestSuggest() {
+    $("#loader").css('display', 'inline-block');
+    var callback = (getParameterByName("m") == "1") ? "displayMobileSuggest" : "displayDesktopSuggest";
+
+    var label = "news";
+    if (location.href.indexOf("label/news") > 0) label = "media";
+    $.get('/feeds/posts/default/-/' + label + '?alt=json-in-script&max-results=6&callback=' + callback);
+}
+
+
+
+function getMetaContent(e) {
+
+    for (var t = document.getElementsByTagName("meta"), n = 0; n < t.length; n++)if (t[n].getAttribute("property") === e) return t[n].getAttribute("content"); return "";
+}
+
+$(document).on("click touch", ".fb-share, .zalo-share", function () {
+
+    var e = getMetaContent("og:title"),
+        n = getMetaContent("og:description"),
+        i = getMetaContent("og:url");
+
+
+    var r = $(this).attr("data-type"), o = $(this).attr("data-href");
+    var title = getMetaContent("og:title");
+
+    switch (window.ga && window.ga("send", {
+        hitType: "event",
+        eventCategory: "mobile_" + r,
+        eventAction: "share",
+        eventLabel: getMetaContent("og:title")
+    }), r) {
+        case "facebook":
+            if ("undefined" !== window.FB) {
+                var a = window.FB;
+
+                a.ui({
+                    method: "share",
+                    href: o
+                }, function (e) { })
+            }
+            break;
+        default:
+    }
+})
+
+
+//Load more trang danh sách
+function listLoadMore() {
+        allowLoadMore = true;
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 2000 && allowLoadMore) {
+                requestLoadMore();
+            }
+        });
+
+}
+
+function requestLoadMore() {
+    $("#loader").css('display', 'inline-block');
+
+    allowLoadMore = false;
+    var nextLink = $("#next-button").attr('href');
+    $.get(nextLink, function (response) {
+        var responseDOM = $(response);
+
+        $("#post-list").append(responseDOM.find("#post-list").html());
+        nextPage = responseDOM.find("#next-button").attr('href');
+        $("#next-button").attr('href', nextPage);
+
+        $("#loader").css('display', 'none');
+
+        allowLoadMore = true;
+
+        $("video:not([parsed='true'])").each(function () {
+            assignVideoHandler(this);
+        });
+
+        eval(responseDOM.find("#ids").html());
+        getArticleStatistics();
+    });
+}
+
+function insertCommentBox(e, t) {
+    $("#comment-area-" + e).html("<div class='fb-comments' data-href='" + t + "' data-numposts='2' data-width='100%'></div>"),
+        FB.XFBML.parse(document.getElementById("comment-area-" + e))
+}
+
+function getArticleStatistics() {
+    if (typeof (ids) != "undefined") {
+        var keyIndex = [];
+        var queryGraph = "https://graph.facebook.com/?ids=";
+
+        for (var i = 0; i < ids.length; i++) {
+            if ($("#id-" + ids[i] + " h3 a").length > 0) {
+                var postUrl = $("#id-" + ids[i] + " h3 a")[0].href;
+                queryGraph += postUrl + ",";
+                keyIndex.push(postUrl);
+            }
+        }
+
+        queryGraph = queryGraph.substring(0, queryGraph.length - 1)
+        $.ajax({
+            url: queryGraph,
+            type: "GET",
+            success: function (data) {
+                for (var i = 0; i <= keyIndex.length - 1; i++) {
+                    var record = data[keyIndex[i]];
+                    if (typeof (record.share) != "undefined") {
+                        if (document.getElementById('total-share-' + ids[i]) != null) {
+                            var likeContent = (record.share.share_count == 0) ? "" : (record.share.share_count + " Chia sẽ");
+                            if (likeContent != "") {
+                                $('#s-c-' + ids[i]).show();
+                                document.getElementById('total-share-' + ids[i]).innerHTML = "<a href='" + keyIndex[i] + "'>" + likeContent + "</a>";
+                                $('#total-share-' + ids[i]).addClass('has-share');
+                            }
+                        }
+
+                        if (document.getElementById('total-comments-' + ids[i]) != null && record.share.comment_count > 0) {
+                            $('#total-comments-' + ids[i]).addClass('has-comment');
+                            document.getElementById('total-comments-' + ids[i]).innerHTML = record.share.comment_count + " Bình luận";
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ mobile.js
@@ -0,0 +1,391 @@
+﻿
+$.fn.videoPlayer = function () {
+    return this.each(function () {
+        new GifPlayer(this);
+    })
+};
+
+function GifPlayer(element) {
+    this.videoLink = null;
+    this.element = element;
+
+    this.loadVideo = function () {
+        var videoLink = $(this.element).attr('data-mp4');
+
+        if (videoLink == undefined) {
+            var postId = $(this.element).attr('data-PostId');
+            var me = this;
+            $.get("https://graph.facebook.com/v2.6/" + postId + "?fields=source&access_token=992872310825515|53t0QsTdyGFiIavgoJMac9qEkIA", function (response) {
+                me.videoLink = response.source;
+                me.playVideo();
+            });
+        }
+        else {
+            this.videoLink = videoLink;
+            this.playVideo();
+        }
+    }
+
+    this.playVideo = function () {
+        var video = $('<video controls="" loop="loop"  controlslist="nodownload" poster="' + $(this.element).attr('src') + '"><source type="video/mp4" src="' + this.videoLink + '"  ></video>');
+
+        $(this.element).replaceWith(video);
+    }
+
+    this.loadVideo();
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function extractThumbnail(content) {
+    var thumbnail = "";
+
+    var findUrl = false;
+    for (var i = 0; i < 500; i++) {
+        if (!findUrl) {
+            if (content[i] == "'") {
+                findUrl = true;
+            }
+
+        } else {
+
+            if (content[i] == "'" && findUrl) {
+                break;
+            }
+
+            thumbnail += content[i];
+        }
+    }
+
+    return thumbnail;
+}
+
+//Load more trang danh sách
+displayNumber = 8;
+loadTimes = 0
+postMaxPublished = "";
+
+readDetaiTimes = 0;
+// #region Xem chi tiết bài viết
+
+isDetailAjaxPage = false; // Xác định đang mở xem chi tiết = ajax
+currentState = "index";
+$(document).on('click', '#post-list a[data-id]', function (e) {
+    e.preventDefault();
+
+    openDetailDialog($(this).attr('data-id'), $(this).attr('href'));
+});
+
+// #region Chia sẽ vào mạng xã hội
+function getMetaContent(e) {
+    for (var t = document.getElementsByTagName("meta"), n = 0; n < t.length; n++)if (t[n].getAttribute("property") === e) return t[n].getAttribute("content"); return "";
+}
+
+$(document).on("click touch", ".fb-share, .zalo-share", function () {
+
+    var e = getMetaContent("og:title"),
+        n = getMetaContent("og:description"),
+        i = getMetaContent("og:url");
+
+
+    var r = $(this).attr("data-type"), o = $(this).attr("data-href");
+    var title = getMetaContent("og:title");
+
+    switch (window.ga && window.ga("send", {
+        hitType: "event",
+        eventCategory: "mobile_" + r,
+        eventAction: "share",
+        eventLabel: getMetaContent("og:title")
+    }), r) {
+        case "facebook":
+            if ("undefined" !== window.FB) {
+                var a = window.FB;
+
+                a.ui({
+                    method: "share",
+                    href: o
+                }, function (e) { })
+            }
+            break;
+        default:
+    }
+})
+// #endregion
+blurDate = new Date();
+
+
+$(document).ready(function () {
+
+    $("video").on('play', function () {
+        var poster = this.getAttribute('poster');
+
+        $("video[poster!='" + poster + "']").each(function () {
+            $(this).get(0).pause();
+        });
+    });
+
+    $("video").each(function () {
+        this.setAttribute('preload', 'metadata');
+    });
+
+    // Chia sẽ bài viết qua facebook
+    $("#facebook-share-anchor").on('click', function () {
+        window.FB.ui({
+            method: "share",
+            href: $(this).attr('data-href')
+        }, function (e) {
+            closeShareMenu();
+        });
+    });
+
+
+    $("video:not([parsed='true'])").each(function () {
+        assignVideoHandler(this);
+    });
+
+    listLoadMore();
+
+    getArticleStatistics();
+
+
+    window.onfocus = function () {
+        var differentSeconds = (new Date().getTime() - blurDate.getTime()) / 1000;
+        // User focus sau 30 phút thì thông báo có bài mới
+        if (differentSeconds > (60 * 30)) {
+            $("body").append("<a class='new-notification' href='http://www.tatlon.com/?m=1'><div>Có hơn 10+ bài viết mới <span class='highlight-text'>Xem</span></div></a>");
+        }
+    };
+
+    window.onblur = function () {
+        blurDate = new Date();
+    };
+    if (location.href.indexOf(".html") >= 0) {
+        $(".gifplayer").videoPlayer(); // play video nếu có
+    }
+});
+
+//Load more trang danh sách
+function listLoadMore() {
+    if (location.href.indexOf("/label/news") < 0) {
+
+        allowLoadMore = true;
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 1500 && allowLoadMore) {
+                requestLoadMore();
+            }
+        });
+    }
+}
+
+function requestLoadMore() {
+    $("#loader").css('display', 'inline-block');
+
+    allowLoadMore = false;
+    var nextLink = $("#next-button").attr('href');
+    nextLink += "&m=1";
+    $.get(nextLink, function (response) {
+        var responseDOM = $(response);
+
+        $("#post-list").append(responseDOM.find("#post-list").html());
+        nextPage = responseDOM.find("#next-button").attr('href');
+        $("#next-button").attr('href', nextPage);
+
+        $("#loader").css('display', 'none');
+
+        allowLoadMore = true;
+
+        $("video:not([parsed='true'])").each(function () {
+            assignVideoHandler(this);
+        });
+
+
+        eval(responseDOM.find("#ids").html());
+
+        getArticleStatistics();
+    });
+}
+function assignVideoHandler(element) {
+
+    $(element).attr('parsed', true);
+    $(element).attr('loop', true);
+
+    $(element).on('inview', function (event, isInView) {
+
+        var postId = $(this).attr('id');
+
+        if (isInView) {
+            // element is now visible in the viewport
+            if (this.paused == true && this.muted == false) {
+                this.muted = true;
+
+                // Chuyển nút điều khiển âm lượng về chế độ tắt
+                if (postId != "") {
+                    $("#sound-" + postId).addClass('off');
+                    $("#sound-" + postId).removeClass('on');
+                }
+            }
+
+            if ($(this).attr('data-autoplay') == "true") {
+                this.muted = false;
+
+                // Chuyển nút điều khiển âm lượng về chế độ mở
+                if (postId != "") {
+                    $("#sound-" + id).addClass('on');
+                    $("#sound-" + id).removeClass('off');
+                }
+            }
+
+            this.play();
+        } else {
+            this.pause()
+        }
+    });
+}
+
+function toggleSearchBar() {
+    var x = document.getElementById("search-bar");
+    if (x.style.display === "none" || x.style.display == "") {
+        x.style.display = "block";
+
+        document.getElementById("search-box").focus();
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function closeShareMenu() {
+    previousState = "";
+    $('#share-action-menu').removeClass('visible');
+}
+
+function openShareMenu(url) {
+    previousState = 'share-dialog';
+    history.pushState({ event: 'share-dialog', url: url }, null, "#share");
+
+    $("#share-action-menu").addClass('visible');
+    $("#facebook-share-anchor").attr('data-href', url);
+    $("#zalo-share-anchor").attr('data-href', url);
+    $("#zalo-share-anchor").attr('data-shareurl', url);
+    $("#direct-link-anchor").attr('href', url);
+}
+
+function openCommentDialog(url) {
+    previousState = "view-comment";
+    history.pushState({ event: 'view-comment', url: url }, null, "#comment");
+
+    if (typeof commentDialog == 'undefined') {
+        commentDialog = $("<div class='modal' tabindex='-1' role='dialog'> <div class='modal-dialog' role='document'> <div class='modal-content'> <div class='modal-header'>  <a href='javascript:history.go(-1)' class='back-button'><i class='spr-close'></i></a> </div> <div class='modal-body'> <p>One fine body&hellip;</p> </div> </div> </div> </div>");
+        $("body").append(commentDialog);
+    }
+
+    $(commentDialog).find("div[class='modal-body']").html("<div id='ajax-fb-comment' ><div class='fb-comments'  data-href='" + url + "' data-numposts='7'></div></div>")
+
+    commentDialog.modal('show');
+    FB.XFBML.parse(document.getElementById("ajax-fb-comment"));
+
+
+    if ($("video[playing='true']").length > 0)
+        $("video[playing='true']")[0].pause();
+}
+
+function getArticleStatistics() {
+    if (typeof (ids) != "undefined") {
+        var keyIndex = [];
+        var queryGraph = "https://graph.facebook.com/?ids=";
+
+        for (var i = 0; i < ids.length; i++) {
+            var postUrl = $("#id-" + ids[i] + " a")[0].href;
+            queryGraph += postUrl + ",";
+            keyIndex.push(postUrl);
+        }
+
+        queryGraph = queryGraph.substring(0, queryGraph.length - 1)
+        $.ajax({
+            url: queryGraph,
+            type: "GET",
+            success: function (data) {
+                for (var i = 0; i <= keyIndex.length - 1; i++) {
+                    var record = data[keyIndex[i]];
+                    if (typeof (record.share) != "undefined") {
+
+                        if (document.getElementById('total-likes-' + ids[i]) != null) {
+                            var likeContent = (record.share.share_count == 0) ? "" : (record.share.share_count + " người thích");
+                            if (likeContent != "") {
+                                $('#s-c-' + ids[i]).show();
+                                document.getElementById('total-likes-' + ids[i]).innerHTML = "<a href='" + keyIndex[i] + "'>" + likeContent + "</a>";
+                            }
+                        }
+
+                        if (document.getElementById('total-comments-' + ids[i]) != null && record.share.comment_count > 0) {
+                            $('#s-c-' + ids[i]).show();
+                            document.getElementById('total-comments-' + ids[i]).innerHTML = "<a class='comment-button' href='" + keyIndex[i] + "'>" + record.share.comment_count + " bình luận</a>";
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+
+
+$(document).ready(function () {
+    // construct an instance of Headroom, passing the element
+    var headroom = new Headroom(document.getElementById("sticker"), {
+        "offset": 45,
+        "tolerance": 5,
+        "classes": {
+            "initial": "animated",
+            "pinned": "slideDown",
+            "unpinned": "slideUp"
+        }
+    });
+    // initialise
+    headroom.init();
+
+    // Mở action menu chia sẽ bài viết
+    $(document).on('click', 'a[class="button-share"]', function (e) {
+        e.preventDefault();
+        openShareMenu(this.getAttribute('href'));
+    });
+
+    // Mở action menu chia sẽ bài viết
+    $(document).on('click', 'a[class="comment-button"],a[class="post-link"]', function (e) {
+        e.preventDefault();
+        openCommentDialog(this.getAttribute('href'));
+    });
+    // Overlay đóng action menu
+    $(".shade").on('click', function () { closeShareMenu() });
+});
+
+window.onpopstate = function (event) {
+    if (previousState == 'share-dialog') {
+        closeShareMenu();
+    }
+    // Đóng bình luận
+    else if (previousState == "view-comment") {
+        previousState = "";
+        if ($("video[playing='true']").length > 0)
+            $("video[playing='true']")[0].play();
+
+        commentDialog.modal('hide');
+    }
+    else if (event.state != null && event.state.event == 'share-dialog') {
+        openShareMenu(event.state.url);
+    }
+    else if (event.state != null && event.state.event == 'view-comment') {
+        openCommentDialog(event.state.url);
+    }
+};
